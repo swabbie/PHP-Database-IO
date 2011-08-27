551de40e2a84f6fe0a907a9f08237809f5b0cc02
John Morris <john@jtmorris.net>
2011-08-26 21:19:48 -0700

    altered the heading description

--- dbio_def.php
+++ dbio_def.php
@@ -4,5 +4,8 @@
  	Copyright John (swabbie) Morris 2011
  	Email: swabbie@jtmorris.net
  	
- 	This file contains the definition for a Database IO class.
+ 	This file contains the definition for the Database IO class.
  ********************************************/
+ 
+ 
+ 
