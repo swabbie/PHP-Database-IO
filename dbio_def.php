919a24d83293fce1859ec8917fdd608632418f61
John Morris <john@jtmorris.net>
2011-08-26 20:32:55 -0700

    added description header to dbio_def.php

---
+++ dbio_def.php
@@ -0,0 +1,8 @@
+ <?php
+ /********************************************
+ 	Database Input/Output Class
+ 	Copyright John (swabbie) Morris 2011
+ 	Email: swabbie@jtmorris.net
+ 	
+ 	This file contains the definition for a Database IO class.
+ ********************************************/
