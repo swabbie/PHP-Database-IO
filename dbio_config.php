190ab37d37cf2365d86486ae2bb359367fe5a7c9
John Morris <john@jtmorris.net>
2011-08-26 21:20:26 -0700

    added a configuration file

---
+++ dbio_config.php
@@ -0,0 +1,23 @@
+<?php
+/********************************************
+ 	Database Input/Output Class
+ 	Copyright John (swabbie) Morris 2011
+ 	Email: swabbie@jtmorris.net
+ 	
+ 	This file contains the connection information for the database.
+ ********************************************/ 
+ 
+ //	Enter the database server's path.
+ $dbio_host = "localhost";
+ 
+ //	Enter the database connection port for the server.
+ $dbio_port = "";
+ 
+ //	Enter the username used to connect to the database.
+ $dbio_username = "";
+ 
+ //	Enter the password used to connect to the database.
+ $dbio_password = "";
+ 
+ //	Enter the database type.  Valid entries are: "mysql"
+ $dbio_type = "mysql";
